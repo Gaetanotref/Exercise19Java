e27849fcbb4b21ef188c1a96e22456a8d15c229e
Gaetano Trefiletti <gaetano.trefiletti.gt@gmail.com>
2022-12-07 16:28:56 +0100

    Commit init

---
+++ src/PrintYourNameAndSurname.java
@@ -0,0 +1,6 @@
+public class PrintYourNameAndSurname {
+    public static void main(String[] args) {
+        String name="Gaetano",surname="Trefiletti";
+        System.out.println(name+"\n"+surname);
+    }
+}
